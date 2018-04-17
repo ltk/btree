e445f4b677e6989e34beecd749487e1fe54ddac8
Gabe Johnson <gabe@blankslatesystems.com>
2018-04-17 11:40:03 -0600

    B-Tree initial commit

---
+++ btree.cpp
@@ -0,0 +1,27 @@
+// btree.cpp
+
+#include <iostream>
+#include "btree.h"
+
+using namespace std;
+
+void insert(btree*& root, int key) {
+  // TODO
+}
+
+void remove(btree*& root, int key) {
+  // TODO
+}
+
+btree* find(btree*& root, int key) {
+  return NULL; // TODO
+}
+
+int count_nodes(btree*& root) {
+  return 0; // TODO
+}
+
+int count_keys(btree*& root) {
+  return 0; // TODO
+}
+
