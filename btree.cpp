08b4b4cd276c26e993c06403f0c167c8ffb8c0b4
Lawson Kurtz <lawson.kurtz@gmail.com>
2018-10-05 17:22:38 -0600

    Finish find function

--- btree.cpp
+++ btree.cpp
@@ -14,7 +14,33 @@ void remove(btree*& root, int key) {
 }
 
 btree* find(btree*& root, int key) {
-  return NULL; // TODO
+  if (root == NULL) {
+    return NULL;
+  }
+
+  if (root->is_leaf) {
+    return root;
+  }
+
+  // scan the key values of the input node to see if the input key is present.
+  // If it is, return this node.
+  for (int i = 0; i < root->num_keys; i++) {
+    if (root->keys[i] == key) {
+      return root;
+    }
+  }
+
+  // Otherwise, for each key in the input node, if the input key is less than the
+  // key we’re looking at, call `find` again passing in the child node linked from
+  // the left of the key we’re looking at. 
+  for (int j = 0; j < root->num_keys; j++) {
+    if (key < root->keys[j]) {
+      return find(root->children[j], key);
+    }
+  }
+
+  // If we get to the end and still haven’t called `find` again, call `find` passing in the last child.
+  return find(root->children[root->num_keys], key);
 }
 
 int count_nodes(btree*& root) {
